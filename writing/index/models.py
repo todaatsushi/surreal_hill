a3f5e549c6a15aef5512467a9b1191e897713710
Atsushi <todaatsushi0@gmail.com>
2019-03-21 18:25:51 +0000

    inex app models prototype finished

--- writing/index/models.py
+++ writing/index/models.py
@@ -1,6 +1,10 @@
+from django.db import models
+from django.utils import timezone
+
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.search import index
 
 
 class StoryIndexPage(Page):
@@ -8,4 +12,38 @@ class StoryIndexPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname='full'),
+    ]
+
+    subpage_types = ['index.StoryMainPage']
+
+
+class StoryMainPage(Page):
+    published_date = models.DateTimeField(default=timezone.now)
+    synopsis = RichTextField(blank=True)
+
+    search_fields = [
+        index.SearchField('title'),
+        index.SearchField('synopsis'),
+        index.FilterField('published_date'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('synopsis', classname='full'),
+    ]
+
+    parent_page_types = ['index.StoryIndexPage']
+    subpage_types = ['index.ChapterPage']
+
+
+class ChapterPage(Page):
+    published_date = models.DateTimeField(default=timezone.now)
+    order = models.IntegerField(null=True)
+    body = RichTextField(blank=True)
+    story = models.ForeignKey(StoryMainPage, on_delete=models.SET_NULL, null=True)
+
+    search_fields = [
+        index.SearchField('title'),
+        index.SearchField('order'),
+        index.SearchField('body'),
+        index.FilterField('published_date'),
     ]
