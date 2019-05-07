07da74e49605be54f788b72d5b5c6718721e2ae9
Atsushi <todaatsushi0@gmail.com>
2019-05-07 19:52:42 +0100

    update static files

--- writing/writing/settings/base.py
+++ writing/writing/settings/base.py
@@ -164,4 +164,4 @@ WAGTAIL_SITE_NAME = "writing"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://example.com'
+BASE_URL = 'http://zenowatss.pythonanywhere.com'
