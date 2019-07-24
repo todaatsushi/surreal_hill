71344e80bb3a2f0cb2032d524ee79896d86f3107
Atsushi <todaatsushi0@gmail.com>
2019-07-24 16:14:12 +0100

    Rename settings files to fix colelctstatic error

--- writing/writing/settings/__init__.py
+++ writing/writing/settings/__init__.py
@@ -2,6 +2,6 @@ import socket
 import os
 
 if socket.gethostname()==os.environ.get('LOCAL_HOST'):
-    from .dev import *
+    from .development import *
 else:
-    from .prod import *
+    from .production import *
